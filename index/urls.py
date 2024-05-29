eed09c41fc1dd93f1a5b57faf4fdd12902c31189
Hishgeebn <khishithug@gmail.com>
2024-05-29 11:53:47 +0800

    user

--- index/urls.py
+++ index/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('profile/', users_views.profile, name='profile'),
     path('register/', users_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', users_views.logout,  name='logout'),
     path('', include('blog.urls')),
 ]
 
